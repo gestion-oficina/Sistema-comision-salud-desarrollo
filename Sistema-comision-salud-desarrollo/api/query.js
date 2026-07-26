bd26745158914847ce2988381423c9979b3deb6e
gestion-oficina <distribuidora205020@gmail.com>
2026-07-26 17:38:04 -0400

    Update query.js

--- Sistema-comision-salud-desarrollo/api/query.js
+++ Sistema-comision-salud-desarrollo/api/query.js
@@ -1,40 +1,57 @@
 import { Database } from "@sqlitecloud/drivers";
 
-const db = new Database(process.env.SQLITECLOUD_URL);
-
 export default async function handler(req, res) {
+
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
 
   if (req.method === "OPTIONS") {
     return res.status(200).end();
   }
 
+
   if (req.method !== "POST") {
     return res.status(405).json({
-      error: "Método no permitido",
+      error: "Método no permitido"
     });
   }
 
+
   try {
+
+    const db = new Database(process.env.SQLITECLOUD_URL);
+
+
     const { query, params = [] } = req.body;
+
 
     if (!query) {
       return res.status(400).json({
-        error: "No se recibió ninguna consulta SQL.",
+        error: "No se recibió ninguna consulta SQL"
       });
     }
 
-    const resultado = await db.sql(query, ...params);
+
+    const resultado = await db.sql(
+      query,
+      params
+    );
+
 
     return res.status(200).json(resultado);
 
-  } catch (error) {
-    console.error(error);
+
+  } catch(error) {
+
+    console.error("ERROR SQLITE CLOUD:", error);
+
 
     return res.status(500).json({
-      error: error.message,
+      error: error.message
     });
+
   }
+
 }
