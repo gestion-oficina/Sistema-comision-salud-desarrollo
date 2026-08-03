1a5a10ba8a18207ca38413bdef399eb3326ef945
gestion-oficina <distribuidora205020@gmail.com>
2026-08-03 16:43:16 -0400

    Create session-manager.js

---
+++ Sistema-comision-salud-desarrollo/js/session-manager.js
@@ -0,0 +1,60 @@
+// ======================================
+// SESSION MANAGER
+// Comisión Permanente de Salud
+// ======================================
+
+// Tiempo máximo de inactividad
+const TIEMPO_INACTIVIDAD = 60000; // 1 minuto
+
+let temporizadorSesion;
+
+// Reinicia el contador
+function reiniciarTemporizadorSesion(){
+
+    clearTimeout(temporizadorSesion);
+
+    temporizadorSesion = setTimeout(cerrarSesionPorInactividad, TIEMPO_INACTIVIDAD);
+
+}
+
+// Cerrar sesión
+function cerrarSesionPorInactividad(){
+
+    sessionStorage.removeItem("usuario_logueado");
+
+    alert("Su sesión ha expirado por inactividad.");
+
+    window.location.replace("login.html");
+
+}
+
+// Eventos que mantienen viva la sesión
+[
+    "mousemove",
+    "mousedown",
+    "click",
+    "keypress",
+    "scroll",
+    "touchstart",
+    "touchmove"
+].forEach(evento=>{
+
+    document.addEventListener(evento,reiniciarTemporizadorSesion,true);
+
+});
+
+// Si cambia de pestaña
+document.addEventListener("visibilitychange",()=>{
+
+    if(document.visibilityState==="visible"){
+        reiniciarTemporizadorSesion();
+    }
+
+});
+
+// Al cargar
+window.addEventListener("load",()=>{
+
+    reiniciarTemporizadorSesion();
+
+});
